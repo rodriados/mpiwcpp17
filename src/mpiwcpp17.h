bccdac2e86a8958ac32c75a1459f149494c8289f
Rodrigo Siqueira <rodriados@gmail.com>
2022-04-20 03:21:28 -0400

    [readme] Updating project's README and creating main header file

---
+++ src/mpiwcpp17.h
@@ -0,0 +1,22 @@
+/**
+ * A thin C++17 wrapper for MPI.
+ * @file The project's main header file.
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2022-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <mpiwcpp17/version.h>
+#include <mpiwcpp17/environment.hpp>
+
+#include <mpiwcpp17/global.hpp>
+
+#include <mpiwcpp17/collective.hpp>
+#include <mpiwcpp17/communicator.hpp>
+#include <mpiwcpp17/datatype.hpp>
+#include <mpiwcpp17/error.hpp>
+#include <mpiwcpp17/exception.hpp>
+#include <mpiwcpp17/functor.hpp>
+#include <mpiwcpp17/process.hpp>
+#include <mpiwcpp17/status.hpp>
+#include <mpiwcpp17/tag.hpp>
