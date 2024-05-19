f141157300bdd56c97a8752163c4977566ed4da5
Rodrigo Siqueira <rodriados@gmail.com>
2024-05-18 20:15:46 -0400

    [thirdparty] Inclusion of thirdparty libraries

--- makefile
+++ makefile
@@ -6,9 +6,12 @@ NAME = mpiwcpp17
 
 INCDIR  = src
 SRCDIR  = src
-OBJDIR  = obj
-TGTDIR  = bin
 TESTDIR = test
+
+DSTDIR ?= dist
+OBJDIR ?= obj
+BINDIR ?= bin
+PT3DIR ?= thirdparty
 
 GCPP   ?= mpic++
 STDCPP ?= c++17
@@ -19,25 +22,74 @@ FLAGS ?=
 GCPPFLAGS ?= -std=$(STDCPP) -I$(INCDIR) -I$(TESTDIR) $(FLAGS)
 LINKFLAGS ?= $(FLAGS)
 
+SRCFILES := $(shell find $(SRCDIR) -name '*.h')                                \
+            $(shell find $(SRCDIR) -name '*.hpp')
 TESTFILES := $(shell find $(TESTDIR) -name '*.cpp')
 TESTDEPS = $(TESTFILES:$(TESTDIR)/%.cpp=$(OBJDIR)/%.test.o)
 
+# The operational system check. At least for now, we assume that we are always running
+# on a Linux machine. Therefore, a disclaimer must be shown if this is not true.
+SYSTEMOS := $(shell uname)
+SYSTEMOS := $(patsubst MINGW%,Windows,$(SYSTEMOS))
+SYSTEMOS := $(patsubst MSYS%,Msys,$(SYSTEMOS))
+SYSTEMOS := $(patsubst CYGWIN%,Msys,$(SYSTEMOS))
+
+ifneq ($(SYSTEMOS), Linux)
+  $(info Warning: This makefile assumes OS to be Linux.)
+endif
+
+# If running an installation target, a prefix variable is used to determine where
+# the files must be copied to. In this context, a default value must be provided.
+ifeq ($(PREFIX),)
+	PREFIX := /usr/local
+endif
+
 all: testing
 
-install:
-	@mkdir -p $(TGTDIR)
+prepare-distribute:
+	@mkdir -p $(DSTDIR)
+
+MPIWCPP17_DIST_CONFIG ?= .packconfig
+MPIWCPP17_DIST_TARGET ?= $(DIST)/$(NAME).h
+
+distribute: prepare-distribute thirdparty-distribute $(MPIWCPP17_DIST_TARGET)
+no-thirdparty-distribute: prepare-distribute $(MPIWCPP17_DIST_TARGET)
+
+clean-distribute: thirdparty-clean
+	@rm -f $(MPIWCPP17_DIST_TARGET)
+	@rm -rf $(DSTDIR)
+
+INSTALL_DESTINATION ?= $(DESTDIR)$(PREFIX)/include
+INSTALL_TARGETS = $(SRCFILES:$(SRCDIR)/%=$(INSTALL_DESTINATION)/%)
+
+install: thirdparty-install $(INSTALL_TARGETS)
+
+$(INSTALL_DESTINATION)/%: $(SRCDIR)/%
+	install -m 644 -D -T $< $@
+
+uninstall: thirdparty-uninstall
+	@rm -f $(INSTALL_TARGETS)
+
+prepare-build:
+	@mkdir -p $(BINDIR)
 	@mkdir -p $(OBJDIR)
 	@mkdir -p $(sort $(dir $(TESTDEPS)))
 
 testing: override FLAGS = -g -O0
-testing: install $(TGTDIR)/runtest.o
+testing: prepare-build $(BINDIR)/runtest.o
 
 runtest: testing
-	mpirun --host localhost:$(np) -np $(np) $(TGTDIR)/runtest.o $(scenario)
+	mpirun --host localhost:$(np) -np $(np) $(BINDIR)/runtest.o $(scenario)
 
-clean:
-	@rm -rf $(TGTDIR)
+clean: clean-distribute
+	@rm -rf $(BINDIR)
 	@rm -rf $(OBJDIR)
+
+.PHONY: all install prepare-build testing runtest uninstall clean
+.PHONY: prepare-distribute distribute no-thirdparty-distribute
+
+$(MPIWCPP17_DIST_TARGET): $(SRCFILES)
+	python pack.py -c $(MPIWCPP17_DIST_CONFIG) -o $@
 
 # Creates dependency on header files. This is valuable so that whenever a header
 # file is changed, all objects depending on it will be recompiled.
@@ -45,10 +97,47 @@ ifneq ($(wildcard $(OBJDIR)/.),)
 -include $(shell find $(OBJDIR) -name '*.d')
 endif
 
-$(TGTDIR)/runtest.o: $(TESTDEPS)
+$(BINDIR)/runtest.o: $(TESTDEPS)
 	$(GCPP) $(LINKFLAGS) $^ -o $@
 
 $(OBJDIR)/%.test.o: $(TESTDIR)/%.cpp
 	$(GCPP) $(GCPPFLAGS) -MMD -c $< -o $@
 
-.PHONY: all install testing runtest clean
+# The target path for third party dependencies' distribution files. As each dependency
+# may allow different settings, a variable for each one is needed.
+export SUPERTUPLE_DIST_TARGET ?= $(DSTDIR)/supertuple.hpp
+export REFLECTOR_DIST_TARGET  ?= $(DSTDIR)/reflector.hpp
+
+THIRDPARTY_DEPENDENCIES ?= supertuple reflector
+THIRDPARTY_TARGETS = $(SUPERTUPLE_DIST_TARGET) $(REFLECTOR_DIST_TARGET)
+
+thirdparty-distribute: prepare-distribute $(THIRDPARTY_TARGETS)
+thirdparty-install:    $(THIRDPARTY_DEPENDENCIES:%=thirdparty-install-%)
+thirdparty-uninstall:  $(THIRDPARTY_DEPENDENCIES:%=thirdparty-uninstall-%)
+thirdparty-clean:      $(THIRDPARTY_DEPENDENCIES:%=thirdparty-clean-%)
+
+export REFLECTOR_DIST_STANDALONE = 1
+
+$(SUPERTUPLE_DIST_TARGET):
+ifndef SKIP_SUPERTUPLE_DISTRIBUTE
+	@$(MAKE) --no-print-directory -C $(PT3DIR)/supertuple distribute
+	cp $(PT3DIR)/supertuple/$@ $@
+endif
+
+$(REFLECTOR_DIST_TARGET):
+ifndef SKIP_REFLECTOR_DISTRIBUTE
+	@$(MAKE) --no-print-directory -C $(PT3DIR)/reflector distribute
+	cp $(PT3DIR)/reflector/$@ $@
+endif
+
+thirdparty-install-%: %
+	@$(MAKE) --no-print-directory -C $(PT3DIR)/$< install
+
+thirdparty-uninstall-%: %
+	@$(MAKE) --no-print-directory -C $(PT3DIR)/$< uninstall
+
+thirdparty-clean-%: %
+	@$(MAKE) --no-print-directory -C $(PT3DIR)/$< clean
+
+.PHONY: thirdparty-distribute thirdparty-install thirdparty-uninstall thirdparty-clean
+.PHONY: $(THIRDPARTY_DEPENDENCIES)
---
+++ pack.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+"""
+A thin C++17 wrapper for MPI.
+@file Source code packing script.
+@author Rodrigo Siqueira <rodriados@gmail.com>
+@copyright 2024-present Rodrigo Siqueira
+"""
+import re, os
+
+from argparse import ArgumentParser
+from configparser import ConfigParser
+from dataclasses import dataclass
+from typing import TextIO
+
+cpp_preprocessor_include_regex = re.compile(rf'^#include *[<"](.*)([>"])$', re.MULTILINE)
+cpp_preprocessor_pragma_once_regex = re.compile(r'^#pragma once$', re.MULTILINE)
+cpp_header_comment_regex = re.compile(r'^/\*\*.*?\*/', re.DOTALL)
+
+source_cleaning_regexs = [
+    cpp_header_comment_regex
+  , cpp_preprocessor_pragma_once_regex
+  , cpp_preprocessor_include_regex
+]
+
+# The information of the project to be packed. Concretely, this type informs which
+# directory the project's source code is located on, what is its namespace and which
+# file is its entrypoint.
+# @since 1.0
+@dataclass
+class ProjectInfo:
+    workingdir: str
+    namespace: str
+    entrypoint: str
+
+# The include dependency graph for a project file. This type separates what dependencies
+# are language-globals and which are project files that must be packed.
+# @since 1.0
+@dataclass
+class IncludeGraph:
+    language: set[str]
+    project: dict[str, list[str]]
+
+# Creates a graph with the include-dependencies of the project's source code files.
+# @param filepath The project's file to create an include graph from.
+# @param project The project's information instance
+# @return The include dependencies graph for the given file.
+def find_include_graph(filepath: str, project: ProjectInfo) -> IncludeGraph:
+    source_code_language_includes = set()
+    source_code_include_graph = dict()
+    unprocessed_include_files = [filepath]
+
+    while unprocessed_include_files:
+        current_file = unprocessed_include_files.pop(0)
+
+        [project_include_files, language_include_files] = \
+            extract_include_dependencies(current_file, project)
+
+        for header in project_include_files:
+            if header not in source_code_include_graph:
+                unprocessed_include_files.append(header)
+
+        source_code_include_graph[current_file] = project_include_files
+        source_code_language_includes |= set(language_include_files)
+
+    return IncludeGraph(
+        project = source_code_include_graph
+      , language = source_code_language_includes)
+
+# Extracts the include-dependencies of a project file.
+# @param filename The file to be analyzed and have its dependencies extracted.
+# @param project The project's information instance.
+# @return A tuple with the file's language-global and project dependencies.
+def extract_include_dependencies(filename: str, project: ProjectInfo) -> tuple:
+    with open(filename, 'r') as fhandle:
+        source_code = fhandle.read()
+
+    language_include_files = []
+    project_include_files = []
+
+    for match in re.finditer(cpp_preprocessor_include_regex, source_code):
+        if match.group(2) == '"':
+            current_directory = os.path.dirname(filename)
+            source_file_path = os.path.join(current_directory, match.group(1))
+            project_include_files.append(os.path.abspath(source_file_path))
+
+        elif project.namespace in match.group(1):
+            source_file_path = os.path.join(project.workingdir, match.group(1))
+            project_include_files.append(os.path.abspath(source_file_path))
+
+        else: language_include_files.append(match.group(1))
+
+    return (project_include_files, language_include_files)
+
+# Iterates over the graph and finds the required include order from the given file.
+# @param srcfile The file to iterate over the graph as an entrypoint.
+# @param graph The project's include graph instance.
+# @return The require include order from the given entrypoint.
+def find_graph_required_order(srcfile: str, graph: IncludeGraph) -> list[str]:
+    source_dependencies = []
+
+    for dependency in graph.project[srcfile]:
+        transitive_dependencies = [] if dependency in source_dependencies \
+            else find_graph_required_order(dependency, graph)
+
+        for transitive_dependency in transitive_dependencies:
+            if transitive_dependency not in source_dependencies:
+                source_dependencies.append(transitive_dependency)
+
+    return source_dependencies + [srcfile]
+
+# Copies the contents of a given source file to an output file.
+# @param srcfile The path of source file to have its contents copied.
+# @param outfhandle The target output file handle to copy source to.
+def copy_code_to_file(srcfile: str, outfhandle: TextIO) -> None:
+    with open(srcfile, 'r') as fhandle:
+        source_contents = fhandle.read()
+
+    for regex in source_cleaning_regexs:
+        source_contents = re.sub(regex, str(), source_contents)
+
+    source_contents = '\n'.join([line for line in source_contents.splitlines() if line])
+
+    print(source_contents, file = outfhandle)
+
+# Copies the whole project's source code into a compacted file.
+# @param outfile The target file to be written with the project source code.
+# @param graph The project files' include-dependency graph.
+# @param project The project information instance.
+def write_compacted_source_code(outfile: str, graph: IncludeGraph, project: ProjectInfo) -> None:
+    namespace = project.namespace.upper()
+    include_order = find_graph_required_order(project.entrypoint, graph)
+
+    with open(project.entrypoint, 'r') as fhandle:
+        entrypoint_header = re                               \
+            .match(cpp_header_comment_regex, fhandle.read()) \
+            .group(0)
+
+    with open(outfile, 'w') as fhandle:
+        print(entrypoint_header, file = fhandle)
+
+        print(f'#ifndef {namespace}_HEADER_INCLUDED', file = fhandle)
+        print(f'#define {namespace}_HEADER_INCLUDED', file = fhandle)
+
+        for header in graph.language:
+            print(f'#include <{header}>', file = fhandle)
+
+        for include_file_path in include_order:
+            copy_code_to_file(include_file_path, fhandle)
+
+        print(f'#endif //{namespace}_HEADER_INCLUDED', file = fhandle)
+
+# Compacts the source code of the whole project into a single file.
+# @param outfile The target file to compact the project's source code to.
+# @param project The project information instance.
+def compact_source_code(*, outfile: str, project: ProjectInfo) -> None:
+    destination_file_path = os.path.abspath(outfile)
+    entrypoint_file_path = os.path.abspath(
+        os.path.join(project.workingdir, project.entrypoint))
+    project_include_graph = find_include_graph(entrypoint_file_path, project)
+
+    write_compacted_source_code(
+        outfile = destination_file_path
+      , graph = project_include_graph
+      , project = ProjectInfo(
+            workingdir = project.workingdir
+          , namespace = project.namespace
+          , entrypoint = entrypoint_file_path))
+
+if __name__ == '__main__':
+    config = ConfigParser()
+    parser = ArgumentParser(description = "C++ source code packing script")
+
+    parser.add_argument('-c', '--config',
+        help = 'The source packing script configuration file',
+        nargs = '?', metavar = 'file', dest = 'config', default = '.packconfig')
+
+    parser.add_argument('-o', '--outfile',
+        help = 'The target file to output the packed source code to',
+        nargs = '?', metavar = 'file', dest = 'outfile')
+
+    args = parser.parse_args()
+    config.read(args.config)
+
+    compact_source_code(
+        outfile = config['output']['outfile'] if args.outfile is None else args.outfile,
+        project = ProjectInfo(
+            workingdir = config['project']['workingdir']
+          , namespace = config['project']['namespace']
+          , entrypoint = config['project']['entrypoint']))
