311300795dfe92a4bd92706a55b397e52d407823
Rodrigo Siqueira <rodriados@gmail.com>
2024-05-21 03:55:44 -0400

    [thirdparty] Updating thirdparty dependencies and wrappers

---
+++ src/mpiwcpp17/thirdparty/reflector.hpp
@@ -0,0 +1,39 @@
+/**
+ * A thin C++17 wrapper for MPI.
+ * @file The include file for reflector thirdparty library
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2022-present Rodrigo Siqueira
+ */
+#pragma once
+
+#include <mpiwcpp17/environment.h>
+
+/*
+ * Determines features that must be disabled depending on the current language version
+ * available or compiler used for compilation.
+ */
+#if MPIWCPP17_CPP_DIALECT < 2014                            \
+ || (MPIWCPP17_COMPILER != MPIWCPP17_OPT_COMPILER_GCC       \
+ &&  MPIWCPP17_COMPILER != MPIWCPP17_OPT_COMPILER_CLANG)
+  #ifndef MPIWCPP17_AVOID_REFLECTION
+    #define MPIWCPP17_AVOID_REFLECTION
+  #endif
+#endif
+
+#ifdef MPIWCPP17_AVOID_REFLECTION
+  #ifndef REFLECTOR_AVOID_LOOPHOLE
+    #define REFLECTOR_AVOID_LOOPHOLE
+  #endif
+#endif
+
+/*
+ * As the reflector thirdparty library depends on the supertuple library to work,
+ * we cannot include it if the latter should be avoided.
+ */
+#ifdef MPIWCPP17_AVOID_THIRDPARTY_SUPERTUPLE
+  #define MPIWCPP17_AVOID_THIRDPARTY_REFLECTOR
+#endif
+
+#ifndef MPIWCPP17_AVOID_THIRDPARTY_REFLECTOR
+  #include <reflector.hpp>
+#endif
---
+++ src/mpiwcpp17/thirdparty/supertuple.hpp
@@ -0,0 +1,11 @@
+/**
+ * A thin C++17 wrapper for MPI.
+ * @file The include file for supertuple thirdparty library
+ * @author Rodrigo Siqueira <rodriados@gmail.com>
+ * @copyright 2022-present Rodrigo Siqueira
+ */
+#pragma once
+
+#ifndef MPIWCPP17_AVOID_THIRDPARTY_SUPERTUPLE
+  #include <supertuple.hpp>
+#endif
